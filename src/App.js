227fe97ab0c1e129d92e29c07a1f7c843cc16c90
Hasib787 <mr.hasib45@gmail.com>
2021-03-19 22:39:25 +0600

    added router for all components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+  useHistory,
+  useLocation
+} from "react-router-dom";
 import './App.css';
+import Blog from "./components/Blog/Blog"; 
+import Contact from "./components/Contact/Contact";
+import Destination from "./components/Destination/Destination";
 import Header from './components/Header/Header';
+import Home from "./components/Home/Home";
+import Login from "./components/Login/Login";
+import NotFound from "./components/NotFound/NotFound";
 
 function App() {
   return (
-    <div >
+    <Router>
       <Header />
-      
-    </div>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/home">
+          <Home />
+        </Route>
+        <Route path="/destination">
+          <Destination />
+        </Route>
+        <Route path="/blog">
+          <Blog />
+        </Route>
+        <Route path="/contact">
+          <Contact />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="*">
+          <NotFound />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -3,7 +3,7 @@ import React from 'react';
 const NotFound = () => {
     return (
         <div>
-            <h1 style={{color: 'red'}}>404...!! Page Is NotFound </h1>
+            <h1 style={{color: 'red', textAlign:'center'}}>404...!! Page Is NotFound </h1>
         </div>
     );
 };
