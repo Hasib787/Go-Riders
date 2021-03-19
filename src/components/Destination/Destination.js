8570b1ac20d47941c6f9abdbd484ef66a34d4131
Hasib787 <mr.hasib45@gmail.com>
2021-03-19 22:13:16 +0600

    added some componets

---
+++ src/components/Destination/Destination.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Destination = () => {
+    return (
+        <div>
+            <h3>This is Destination</h3>
+        </div>
+    );
+};
+
+export default Destination;
