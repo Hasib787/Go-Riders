a2d16c48b4d936f73eebfeae0ee9efd5c563b775
Hasib787 <mr.hasib45@gmail.com>
2021-03-22 22:57:35 +0600

    design destination

--- src/components/Destination/Destination.js
+++ src/components/Destination/Destination.js
@@ -1,12 +1,27 @@
 import React from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
+import { Col, Container, Row, Button } from 'react-bootstrap';
+import './Destination.css';
+import map from '../../images/Map.png'
 
 const Destination = () => {
     return (
         <Container>
             <Row>
-                <Col sm={8}>sm=8</Col>
-                <Col sm={4}>sm=4</Col>
+                <hr/>
+                <Col style={{height: '280px'}} id="searchLocation"  lg={3}>
+                    <form action="">
+                        <p>Pick From</p>
+                        <input type="text"/>
+                        <br/>
+                        <p>Pick To</p>
+                        <input type="text"/>
+                        <br/>
+                        <Button id="searchbtn">Search</Button>
+                    </form>
+                </Col>
+                <Col id="map" lg={8}>
+                        <img src={map} alt=""/>
+                </Col>
             </Row>
         </Container>
     );
