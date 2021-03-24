e0a379b4dbc673829627d1954a6edd70cf3fbead
Hasib787 <mr.hasib45@gmail.com>
2021-03-25 00:16:58 +0600

    added search state

--- src/components/Destination/Destination.js
+++ src/components/Destination/Destination.js
@@ -7,26 +7,21 @@ import './Destination.css';
 const Destination = () => {
     const [pickFrom, setPickFrom] = useState('');
     const [pickTo, setPickTo] = useState('');
+    const [searchClicked, setSearchClicked] = useState();
 
+    const handleSearchBtn=()=>{
+        setSearchClicked(true)
+    }
 
     return (
         <Container>
             <Row>
                 <hr />
-                <Col style={{ height: '280px' }} id="searchLocation" lg={3}>
-                    <form action="">
-                        <p>Pick From</p>
-                        <input onBlur={e => setPickFrom(e.target.value)} type="text" placeholder="Pick From" />
-                        <br />
-
-                        <p>Pick To</p>
-                        <input onBlur={e => setPickTo(e.target.value)} type="text" placeholder="Pick To" />
-                        <br />
-                        <Button id="searchbtn">Search</Button>
-                    </form>
-                </Col >
+               
                 <br/>
-                <Col style={{ height: '360px' }} id="searchLocation" lg={3}>
+                
+                {
+                    searchClicked ? <Col style={{ height: '360px' }} id="searchLocation" lg={3}>
                     <div id="location">
                         <p id="pickFrom">{pickFrom}</p>
                         <p>{pickTo}</p>
@@ -40,7 +35,20 @@ const Destination = () => {
                     <div className="rideDetails">
                         <img src="" alt=""/>
                     </div>
-                </Col>
+                </Col>:  <Col style={{ height: '280px' }} id="searchLocation" lg={3}>
+                    <form action="">
+                        <p>Pick From</p>
+                        <input onBlur={e => setPickFrom(e.target.value)} type="text" placeholder="Pick From" />
+                        <br />
+
+                        <p>Pick To</p>
+                        <input onBlur={e => setPickTo(e.target.value)} type="text" placeholder="Pick To" />
+                        <br />
+                        <Button onClick={handleSearchBtn} id="searchbtn">Search</Button>
+                    </form>
+                </Col >
+  
+                }
                 <Col id="map" lg={8}>
                     <Map></Map>
                 </Col>
